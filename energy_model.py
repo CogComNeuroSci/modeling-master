39024cb1f45ef560cdb9cc406010a1f03d65ba88
Tom Verguts <tom.verguts@ugent.be>
2017-12-28 21:54:54 +0100

    edit energy_model

--- energy_model.py
+++ energy_model.py
@@ -5,6 +5,7 @@ Created on Thu Dec 28 12:15:46 2017
 
 @author: tom
 simple energy minimization model
+with RT simulation
 """
 
 # import
@@ -13,29 +14,46 @@ from numpy import random
 import matplotlib.pyplot as plt
 
 # initialize
-b = [3, 2]
-w_p = 0.1
+n_trials = 100
+b = [3, 0.2]
+w_p = 0.01
 w_m = 1
-theta = 0.99 # decay parameter
+theta = 0.995 # 1-decay parameter
 alpha = 0.01 # change rate
-max_n_step = 1000
-x=[0]
-y=[0]
+max_n_step = 300
 std_noise = 0.1
 threshold=2
-threshold_reached = False
-counter = 0
+rt = []
+accuracy = 0
+too_late = 0
 
-# start
-while not threshold_reached and counter<max_n_step:
-    d_x = alpha*(b[0] + 2*w_p*x[-1] - w_m*y[-1]) + std_noise*random.randn()
-    d_y = alpha*(b[1] + 2*w_p*y[-1] - w_m*x[-1]) + std_noise*random.randn()
-    x.append(theta*x[-1] + d_x)
-    y.append(theta*y[-1] + d_y)
-    x[-1] = maximum(0,x[-1]) # threshold at zero
-    y[-1] = maximum(0,y[-1]) # threshold at zero
-    if maximum(x[-1],y[-1])>=threshold:
-        threshold_reached = True
-    counter += 1    
-plt.plot(range(len(x)),x,"bo-", range(len(y)),y,"ro-")
+for loop in range(n_trials):
+    # start
+    x=[0]
+    y=[0]
+    threshold_reached = False
+    counter = 0
+    while not threshold_reached and counter<max_n_step:
+        d_x = alpha*(b[0] + 2*w_p*x[-1] - w_m*y[-1]) + std_noise*random.randn()
+        d_y = alpha*(b[1] + 2*w_p*y[-1] - w_m*x[-1]) + std_noise*random.randn()
+        x.append(theta*x[-1] + d_x)
+        y.append(theta*y[-1] + d_y)
+        x[-1] = maximum(0,x[-1]) # threshold at zero
+        y[-1] = maximum(0,y[-1]) # threshold at zero
+        if maximum(x[-1],y[-1])>=threshold:
+            threshold_reached = True
+        counter += 1    
+    rt.append(counter)
+    accuracy += (x[-1]>y[-1])    
+    too_late += (counter>=max_n_step)
+
+# plot final trajectory
+f, axarr = plt.subplots(nrows = 2, ncols = 1)
+axarr[0].plot(range(len(x)),x,"bo-", range(len(y)),y,"ro-")
+axarr[0].set_title("random trajectory")
+# plot histogram
+axarr[1].hist(rt)
+axarr[1].set_title("RT distribution")
+print("accuracy: {:2.0%}".format(accuracy/n_trials))
+print("too late: {:2.0%}".format(too_late/n_trials))
 ##
