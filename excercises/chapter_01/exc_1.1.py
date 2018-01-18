69ee92da0900cae37ec8a4141a2f926f612906e8
phuycke <pieter.huycke@hotmail.com>
2018-01-18 18:03:22 +0100

    add folder structure and first file

---
+++ excercises/chapter_01/exc_1.1.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Problem setting
+"""
+Implement an algorithm that minimizes f(x) = A*(x-B)2 + C. 
+Try out different values of A, B, and C. 
+What is their role in the optimization? 
+Try to reason it through first, then implement it.
+"""
+
