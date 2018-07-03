58beaede4555381aadd7e41be479e67c5fee1be4
Tom Verguts <tom.verguts@ugent.be>
2018-07-03 17:39:46 +0200

    to make chap 2 figs

---
+++ ch2_figs.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul  3 13:49:33 2018
+
+@author: tom
+create fig 2.4
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+x = np.linspace(start = -5, stop = 3, num = 20)
+y = (x**4)/4 +(2/3)*(x**3)-(5/2)*(x**2)-6*x
+plt.plot(x,y)
+plt.ylabel("y = f(x)")
