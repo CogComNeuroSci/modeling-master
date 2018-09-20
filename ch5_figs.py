4b661fe4ad928c6ed49811a1a3dfd8a0897b2bb0
Tom Verguts <tom.verguts@ugent.be>
2018-09-20 14:03:24 +0200

    chap 5 figs

--- ch5_figs.py
+++ ch5_figs.py
@@ -12,6 +12,7 @@ import matplotlib.pyplot as plt
 slope = [ -2, 0.5, 3]
 intercept = [0, 1, 1]
 low, high = -1, 2
+low_rbf, high_rbf = -3, 3
 font_size = 5
 
 def conv(x, y, sign = 1, list = range(len(slope))):
@@ -26,6 +27,9 @@ def plotlines():
     for i in range(len(slope)):
         y = slope[i]*x + intercept[i]
         plt.plot(x, y, color = "black")
+
+def rbf(x,y,cen_x,cen_y):
+    return np.exp(-((x-cen_x)**2 + (y-cen_y)**2))
     
 #%% figure 5.2a
 # determine the convex set
@@ -62,6 +66,7 @@ plt.title("Fig 5.2b \nAn OR of convex sets", {"fontsize": font_size})
 # (Code mostly stolen from Pieter)
 
 from numpy import arange
+from mpl_toolkits.mplot3d import Axes3D
 from pylab import meshgrid, cm, title
 from matplotlib.ticker import LinearLocator, FormatStrFormatter
 import matplotlib.pyplot as plt
@@ -74,10 +79,10 @@ def z_func(x, y, nr):
     if nr == 1:
         return (x ** 2 + y ** 2)/10
     else:
-        return ((x/4) ** 4 - ((x/2) ** 2) + (y/4)** 4 - (y/2) ** 2 ) 
+        return (x/4) ** 4 - (x/2) ** 2 + (y/4) ** 4 - (y/2) ** 2  + x/8 + y/8
 
-x = arange(-10.0, 10.0, 0.1)
-y = arange(-10.0, 10.0, 0.1)
+x = arange(-8.0, 8.0, 0.1)
+y = arange(-8.0, 8.0, 0.1)
 
 # grid of point
 X, Y = meshgrid(x, y)
@@ -113,3 +118,15 @@ plt.plot(x,y, color = "black")
 plt.xlabel("x")
 plt.ylabel("y = max{0,x}")
 plt.title("Fig 5.5b \nA novel transformation function", {"fontsize": font_size})
+
+
+#%% figure 5.9 Radial basis functions
+x_center, y_center = 1, 0
+plt.figure()
+plt.title("fig 5.9: \nRadial basis activation function", {"fontsize": font_size})
+ngrid = 100
+xi = np.linspace(low_rbf, high_rbf, ngrid)
+yi = np.linspace(low_rbf, high_rbf, ngrid)
+Xi, Yi = np.meshgrid(xi, yi)
+zi = rbf(Xi,Yi,x_center,y_center)
+plt.contourf(xi, yi, zi, 14)
