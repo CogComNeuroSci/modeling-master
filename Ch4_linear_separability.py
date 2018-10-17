bbc6d3f3c2a0e6fd5dfad19ed01aa6c7a0c71bc1
Tom Verguts <tom.verguts@ugent.be>
2018-10-17 16:45:14 +0200

    chapter 4 new files

--- Ch4_linear_separability.py
+++ Ch4_linear_separability.py
@@ -1,6 +1,7 @@
-# visualize linear and nonlinear mappings in 3D
-# exercise in numpy use
-# run in psychopy environment
+# visualize linear and nonlinear mappings in 3D (choose linear, xor, or stroop)
+# author: tom verguts
+# run this in psychopy environment
+
 # import
 from psychopy import visual,event,gui
 import numpy as np
@@ -16,28 +17,30 @@ pr = np.empty([n_points,n_dim-1]) ## the projection of point into 2D
 for loop in range(2):
     point[0+4*loop,:] = (0,0,loop*size)
     point[1+4*loop,:] = (size,0,loop*size)
-    point[2+4*loop,:] = (size,size,loop*size)
-    point[3+4*loop,:] = (0,size,loop*size)
+    point[2+4*loop,:] = (0,size,loop*size)
+    point[3+4*loop,:] = (size,size,loop*size)
     
 win = visual.Window()
-colormap = ["red","red","green","green","red","red","green","green"]
-xormap = ["red","red","green","green","red","green","green","red"]
+linear_map = ["red","red","green","green","red","red","green","green"] # linearly separable
+xor_map = ["red","green","green","red","red","green","green","red"]    # 2D linearly inseparable 
+stroop_map = ["red","red","green","green","red","green","red","green"] # 3D linearly inseparable  
 theta = 0.1 ## transformation angle
 transfoZl = np.array([[np.cos(theta),-np.sin(theta),0],[np.sin(theta),np.cos(theta),0],[0,0,1]]) ## around Z-axis
 transfoZr = np.array([[np.cos(-theta),-np.sin(-theta),0],[np.sin(-theta),np.cos(-theta),0],[0,0,1]]) ## around Z-axis
-base = np.transpose(np.array([[1,0.2,0.5],[0,0.5,0]])) ## 2D space spanned by base
+base = np.transpose(np.array([[1,1,1],[-1,0,1]])) ## 2D space spanned by base
 proj_matrix = np.dot(np.linalg.inv(np.dot(np.transpose(base),base)),np.transpose(base))
 instruction = visual.TextStim(win,text="Press f, j to rotate left, right; q to quit",pos=(0,0.8))
 
 # ask the mapping
-info = {"mapping?":"color"}
+info = {"mapping?":"linear"}
 dlg = gui.DlgFromDict(info)
 map = info["mapping?"]
-if map=="color":
-    relevant_map = colormap
+if map=="linear":
+    relevant_map = linear_map
+elif map=="xor":
+    relevant_map = xor_map
 else:
-    relevant_map = xormap
-
+    relevant_map = stroop_map
 # and based on that, initialize the circles
 c1 = visual.Circle(win,radius=radius_size,fillColor=relevant_map[0])
 c2 = visual.Circle(win,radius=radius_size,fillColor=relevant_map[1])
