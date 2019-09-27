9ffc000dd3c6c3db0e87d0887525f4ab1cb5865c
Mehdi <mehdi@Mehdis-MacBook-Pro.local>
2019-09-27 19:58:08 +0200

    adding all the scripts mentioned in the lesson 2 course pdf. BEWARE these scripts haven't all been updated so they are not the final scripts that will be used in class. I will do this before monday

---
+++ AY 2019 - 2020/Lesson 02/Practical session/solutions/ch2_pet_detector_network_plot_step1_solution.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@author: Mehdi Senoussi
+"""
+
+import numpy as np
+import time
+from ch0_course_functions import plot_network, update_network
+
+timesleep = .05
+n_tsteps = 50
+times = np.arange(n_tsteps)
+t = 1
+# output units
+ydog = np.zeros(n_tsteps)
+ycat = np.zeros(n_tsteps)
+incat = np.zeros(n_tsteps)
+indog = np.zeros(n_tsteps)
+# std of noise
+sigma = .25
+# learning rate
+alpha = .2
+
+# we have 3 input unit on the first layer and 2 output units on the second layer
+layers = np.array([1, 1, 1, 2, 2])
+n_units = len(layers)
+# here we set the first feature detector ('bites visitors') to 1 and the 2
+# others to 0. We also set the two output units ('cat' and 'dog') to 0 and we
+# will update their activation later
+activations = np.array([1., 0., 0., 0., 0.])
+weights = np.zeros(shape=[n_units, n_units])
+
+# setting up weights for the cat-dog detector
+weights[0, 3] = .8 # cats often bite visitors
+weights[0, 4] = .1 # dogs rarely bite visitors
+weights[1, 3] = .2 # cats often have four legs
+weights[1, 4] = .2 # dogs often have four legs
+weights[2, 3] = .1 # cats rarely have their pictures on FB
+weights[2, 4] = .8 # dogs often have their pictures on FB
+weights[3, 4] = -.2 # a cat cannot be a dog, and vice versa
+
+# computing the initial y activation values
+incat = weights[0, 3] * activations[0] + weights[1, 3] * activations[1] + weights[2, 3] * activations[2]
+indog = weights[0, 4] * activations[0] + weights[1, 4] * activations[1] + weights[2, 4] * activations[2]
+ycat[t] = ycat[t-1] + alpha * (incat + weights[3, 4] * ydog[t-1]) + np.random.randn()*sigma
+ydog[t] = ydog[t-1] + alpha * (indog + weights[3, 4] * ycat[t-1]) + np.random.randn()*sigma
+activations[3:] = [ycat[t], ydog[t]]
+
+# plot the network
+fig, axs, texts_handles, lines_handles, unit_pos =\
+    plot_network(figsize = [13, 7], activations = activations,
+                  weights = weights, layers = layers)
+
