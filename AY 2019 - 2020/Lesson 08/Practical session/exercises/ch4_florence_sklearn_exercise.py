e1a489f2a6342b9fd5950a21cacc6558e2a1c13c
Pieter Huycke <24294920+phuycke@users.noreply.github.com>
2019-11-06 10:53:28 +0100

    update lesson 8

---
+++ AY 2019 - 2020/Lesson 08/Practical session/exercises/ch4_florence_sklearn_exercise.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+
+"""
+@author: Pieter
+Pieter.Huycke@UGent.be
+
+- - - - - - - - - - - - 
+
+Stuck using a function?
+No idea about the arguments that should be defined?
+
+Type:
+help(module_name)
+help(function_name)
+to let Python help you!
+"""
+
+#%%
+
+# import: general and scikit-learn specific
+import numpy              as np
+
+...
+
+#%%
+
+# define the patterns we need to associate
+image_florence   = np.array([.99, .01, .99, .01, .99, .01])
+song_florence    = 1
+
+your_image       = np.array([.01, .99, .01, .99, .01, .99])
+your_song        = 0
+
+# make an input - and an output array
+  # dimensions should be (100, 6) and (100, ) respectively
+...
+
+#%%
+
+# shuffle input - and output array **together**
+...
+
+#%%
+
+# split the data in a training proportion (75% )and a test proportion (25%)
+X_train, y_train, X_test, y_test = ...
+
+
+# use scikit-learn to train your model on the input and its associated target
+...
+
+#%%
+
+# how does your model perform on the 'unseen' observations? 
+...
+
+# print accuracy using the built-in sklearn function accuracy_score()
+...
