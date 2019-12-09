f3589d34b528bb2c66b3f90ad9f68ea796b0a358
Tom Verguts <tom.verguts@ugent.be>
2019-12-09 08:37:49 +0100

    yet more updates

--- AY 2019 - 2020/Lesson 09/Chapter 06/course examples/ch6_curve.py
+++ AY 2019 - 2020/Lesson 09/Chapter 06/course examples/ch6_curve.py
@@ -7,12 +7,19 @@ Created on Sun Dec  8 12:51:49 2019
 show likelihood curve for the learning model
 """
 
+#%% initialize
+import numpy as np
 from ch6_ST_estimate import generate_learn
 
-# generate some data
-generate_learn()
+learning_rate, temperature = 0.6, 0.4 
+low, high, stepsize = -3, +3, .01
+x = np.arange(low, high, stepsize)
+y = np.arange(low, high, stepsize)
+
+#%% generate some data
+generate_learn(alpha = learning_rate, beta = temperature)
 
 # calculate some likelihoods
 # calculate logL_learn over a grid of points
 
-# plot the grid
+#%% plot the grid
