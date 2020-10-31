d0cd39e8f29d3780e459226ceb3fa7f38fe04e2a
Tom Verguts <tom.verguts@ugent.be>
2020-10-31 11:47:00 +0100

    Create ch5_linear_mappings.py

---
+++ code by chapter/Chapter 5/ch5_linear_mappings.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 31 10:52:51 2020
+
+@author: tom
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+w = np.array([[1, 1, 0], [2, -1, 2], [-1.5, 2, 3], [-1, -1, -1]])
+
+fig, axs = plt.subplots(nrows = 2, ncols = 2)
+
+x = np.linspace(-1, 1)
+
+for loop in range(4):
+	row = loop//2
+	col = loop%2
+	y = w[loop, 0]*x + w[loop, 1]
+	axs[row, col].plot(x, y, color = "black")
+	axs[row, col].set_ylim(bottom = -1, top = 1)
