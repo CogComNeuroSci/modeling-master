4c4c8ffacafb871f89b005165c399066327f47a4
Tom Verguts <tom.verguts@ugent.be>
2021-07-02 13:49:32 +0200

    more tf2 code

--- code by chapter/Chapter 4/ch4_tf2_delta_alternative.py
+++ code by chapter/Chapter 4/ch4_tf2_delta_alternative.py
@@ -5,6 +5,7 @@
 @author: tom verguts
 does 2-layer network weight optimization via MSE minimization
 in TF2
+alternative (less elegant) model construction
 """
 
 import tensorflow as tf
@@ -17,10 +18,9 @@ train_x = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
 train_y = np.array([0, 1, 1, 1])
 train_y = train_y.reshape(4,1)
 
-model = tf.keras.Sequential([
-			tf.keras.Input(shape=(2,)),
-			tf.keras.layers.Dense(1)
-			] )
+model = tf.keras.Sequential()
+model.add(tf.keras.Input(shape=(2,)))
+model.add(tf.keras.layers.Dense(1))
 model.build()
 model.compile(optimizer = "adam", loss=tf.keras.losses.MeanSquaredError())
 history = model.fit(train_x, train_y, batch_size = 1, epochs = 500)
--- code by chapter/Chapter 5/ch5_tf2_backprop.py
+++ code by chapter/Chapter 5/ch5_tf2_backprop.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
+Created on Sun Aug 30 21:37:36 2020
 
 @author: tom verguts
-does 2-layer network weight optimization via MSE minimization
-in TF2
-slightly different (more elegant) construction than in ch4_tf2_delta.py
+Applies backprop via MSE minimization
+with TF2 and keras
 """
 
 import tensorflow as tf
@@ -13,17 +13,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 learning_rate = 0.1
-epochs = 100
+opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
+n_hid = 3
 train_x = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
-train_y = np.array([0, 1, 1, 1])
+train_y = np.array([0, 1, 1, 0])
 train_y = train_y.reshape(4,1)
 
 model = tf.keras.Sequential([
 			tf.keras.Input(shape=(2,)),
+			tf.keras.layers.Dense(n_hid),
 			tf.keras.layers.Dense(1)
 			] )
 model.build()
-model.compile(optimizer = "adam", loss=tf.keras.losses.MeanSquaredError())
+model.compile(optimizer = opt, learning_rate = learning_rate, loss=tf.keras.losses.MeanSquaredError())
 history = model.fit(train_x, train_y, batch_size = 1, epochs = 500)
 model.summary()
 
