a57754b4b2b6dc87153c4c817e30d1c75adc71e8
Tom Verguts <tom.verguts@ugent.be>
2021-07-02 14:23:30 +0200

    Update ch5_tf2_backprop.py

--- code by chapter/Chapter 5/ch5_tf2_backprop.py
+++ code by chapter/Chapter 5/ch5_tf2_backprop.py
@@ -12,21 +12,21 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-learning_rate = 0.1
-opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
-n_hid = 3
+learning_rate = 0.001
+opt = tf.keras.optimizers.SGD(learning_rate = learning_rate)
+n_hid = 5
 train_x = np.array([[0, 0], [1, 0], [0, 1], [1, 1]])
 train_y = np.array([0, 1, 1, 0])
 train_y = train_y.reshape(4,1)
 
 model = tf.keras.Sequential([
 			tf.keras.Input(shape=(2,)),
-			tf.keras.layers.Dense(n_hid),
+			tf.keras.layers.Dense(n_hid, activation = "relu"),
 			tf.keras.layers.Dense(1)
 			] )
 model.build()
-model.compile(optimizer = opt, learning_rate = learning_rate, loss=tf.keras.losses.MeanSquaredError())
-history = model.fit(train_x, train_y, batch_size = 1, epochs = 500)
+model.compile(optimizer = opt, loss=tf.keras.losses.MeanSquaredError())
+history = model.fit(train_x, train_y, batch_size = 1, epochs = 10000, verbose = 0)
 model.summary()
 
 # error curve
