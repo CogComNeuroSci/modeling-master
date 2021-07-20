de467c65b920a1e468aeb2a898bd5fd28f1f2b76
Tom Verguts <tom.verguts@ugent.be>
2021-07-20 11:19:35 +0200

    Update ch2_tf2_hopfield_large.py

--- code by chapter/Chapter 2/ch2_tf2_hopfield_large.py
+++ code by chapter/Chapter 2/ch2_tf2_hopfield_large.py
@@ -4,9 +4,8 @@
 Created on Mon July 19, 2021
 
 @author: tom verguts
-TF-based hopfield network for arbitrary vectors X and Y
-Both X, Y, -X, and -Y are attractors in this case.
-Note also distance(X, -X) = 20 (in case N = 100)
+TF-based hopfield network for arbitrary vectors X1, X2, ...
+Both X_i and -X_i are attractors in this case.
 """
 
 import tensorflow as tf
@@ -14,36 +13,44 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # define fixed parameters
-N = 100
-X = 2*(np.random.random(N)>0.5)-1 # +1 / -1 coding
-X = X[:,np.newaxis]
-Y = 2*(np.random.random(N)>0.5)-1 # +1 / -1 coding
-Y = Y[:, np.newaxis]
-
-w    = np.matmul(X, X.T) + np.matmul(Y, Y.T)
+n_units   = 100
+n_stimuli = 3
 threshold = 0
-b    = np.array(N*[threshold])
+learning_rate = 1
+
+# define the stimuli
+X = np.ndarray((n_units, n_stimuli))
+for stimulus_loop in range(n_stimuli):
+	X[:,stimulus_loop] = 2*(np.random.random(n_units)>0.5)-1 # +1 / -1 coding
+
+# construct weight matrix
+w = np.zeros((n_units, n_units))
+for stimulus_loop in range(n_stimuli):
+	w    = w + learning_rate*np.matmul(X[:, stimulus_loop][:,np.newaxis], X[:, stimulus_loop][:,np.newaxis].T)
+b    = np.array(n_units*[threshold])
 b    = b[:, np.newaxis]
-start_pattern = (2*np.random.random(N)>0.5)-1
+
+# initial starting point
+start_pattern = (2*np.random.random(n_units)>0.5)-1
 start_pattern = start_pattern[:, np.newaxis]
 
 n_sample = 50
-distance = np.zeros((2, n_sample))
+distance = np.zeros((n_sample, n_stimuli))
+max_distance = np.sqrt(4*n_units)
 
 # a function to sample the network iteratively
 def hopfield(start_pattern = None, n_sample = 0):
-	pattern = tf.cast(start_pattern, dtype = tf.int64)
+	pattern = tf.cast(start_pattern, dtype = tf.double)
 	for loop in range(n_sample):
-		distance[:,loop] = [np.linalg.norm(X-pattern), np.linalg.norm(Y-pattern)]
-		net_input = tf.matmul(w, pattern) #+ tf.multiply(pattern, b)
+		for stimulus_loop in range(n_stimuli):
+			distance[loop, stimulus_loop] = np.linalg.norm(X[:, stimulus_loop][:,np.newaxis]-pattern)/max_distance
+		net_input = tf.matmul(w, pattern)  + tf.multiply(pattern, b)
 		clipped   = tf.cast(tf.math.greater(net_input, 0), tf.float32)
 		pattern   = clipped*2 - 1
-		pattern = tf.cast(pattern, dtype = tf.int64)
+		pattern = tf.cast(pattern, dtype = tf.double)
 	return pattern
 
 pattern = hopfield(start_pattern = start_pattern, n_sample = n_sample)
-#print(pattern.numpy())
-print("distance from X: ", np.linalg.norm(X-pattern))
-print("distance from Y: ", np.linalg.norm(Y-pattern))
-plt.plot(distance[0,:], color = "red")
-plt.plot(distance[1,:], color = "black")
+print("distance to different stimuli (0-1 scale): ", distance[-1,:])
+for stim_loop in range(n_stimuli):
+	  plt.plot(distance[:, stim_loop], color = "black")
