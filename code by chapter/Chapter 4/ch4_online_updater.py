73c6f35813bf19b758e915e06fe48d0990c2423e
Tom Verguts <tom.verguts@ugent.be>
2021-10-15 12:06:56 +0200

    Update ch4_online_updater.py

--- code by chapter/Chapter 4/ch4_online_updater.py
+++ code by chapter/Chapter 4/ch4_online_updater.py
@@ -11,14 +11,17 @@ can be used to check convergence properties etc
 import numpy as np
 import matplotlib.pyplot as plt
 
-n_trials = 100
-beta, gamma = 0.5, 1
+n_trials = 200
+gamma = 1
 mean, std = 1, 0.00
+beta_list = [0.1, 1, 1.2, -0.5]
 
-w = np.ndarray(n_trials)
+fig, axs = plt.subplots(nrows = 1, ncols = 4)
 
-for loop in range(n_trials):
-	R = np.random.randn()*std + mean
-	w[loop] = w[loop-(loop>1)] + beta*(R - gamma*w[loop-(loop>1)])
-
-plt.plot(w)
+for indx, beta in enumerate(beta_list):
+    w = np.zeros(n_trials)
+    for loop in range(n_trials):
+	    R = np.random.randn()*std + mean
+	    w[loop] = w[loop-(loop>0)] + beta*(R - gamma*w[loop-(loop>0)])
+    axs[indx].plot(w)
+    axs[indx].set_title("beta = {}".format(beta))
