2d0fd28d0877e4fa81167aee4df95e222a32496c
Tom Verguts <tom.verguts@ugent.be>
2022-01-04 16:06:14 +0100

    Update ch10_figs.py

--- code by chapter/Chapter 10/ch10_figs.py
+++ code by chapter/Chapter 10/ch10_figs.py
@@ -5,14 +5,19 @@ Created on Fri Jul  6 10:37:20 2018
 
 @author: tom verguts
 code for pics in chapter 10: unsupervised learning
-(chapter includes Boltzmann)
 """
+#%% imports and initialization
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as ptch
 
-#%% embedded in a cell so it can run on its own
-# fig 11.2b
+# used in 10.2b, 10.2c, and 10.2d; pics in book are a bit more sober than specified here
+cat_color, cat_marker, cat_size = "black", "x", 10
+dog_color, dog_marker, dog_size = "black", "o", 10 
+cluster_color, cluster_marker, cluster_size = "red", "<", 300
+
+#%% fig 10.2b: Data points in a 2D space, with cluster centers added
+# figure-specific initializations
 n_cats = 20
 mean_cats = [-1, -1]
 cov_cats  = np.eye(2)*.2
@@ -22,31 +27,22 @@ cov_dogs  = np.eye(2)*.2
 n_dogs_special = 5
 mean_dogs_special = [0, 3]
 cov_dogs_special  = np.eye(2)*.05
-output1 = [-1.1, -0.9] # final state
-output2 = [0.9, 1.05]  # final state
-#output1 = [-2, 2] # output units in a random initial state
-#output2 = [-1.8, 2.1] # output units in a random initial state
-cat_color = "black"
-cat_marker = "o"
-cat_size = 10
-dog_color = "black"
-dog_marker = "o"
-dog_size = 10
-cluster_color = "black"
-cluster_marker = "X"
-cluster_size= 300
+output1 = [-1.1, -0.9] # final state of the cluster 1
+output2 = [0.9, 1.05]  # final state of the cluster 2
 cats = np.transpose(np.random.multivariate_normal(mean_cats,cov_cats,size = [n_cats]))
 dogs = np.transpose(np.random.multivariate_normal(mean_dogs,cov_dogs,size = [n_dogs]))
 dogs_special = np.transpose(np.random.multivariate_normal(mean_dogs_special,cov_dogs_special,size = [n_dogs_special]))
+
+# plotting
 plt.scatter(cats[0],cats[1], n_cats, c = cat_color, marker = cat_marker)
 plt.scatter(dogs[0],dogs[1], n_dogs, c = dog_color, marker = dog_marker)
 plt.scatter(dogs_special[0], dogs_special[1], n_dogs, c = "black")
 plt.scatter(output1[0], output1[1], s = 200, c = cluster_color, marker = cluster_marker)
 plt.scatter(output2[0], output2[1], s = 200, c = cluster_color, marker = cluster_marker)
-#plt.title("Competitive learning")
+plt.title("Competitive learning")
 
-#%%
-# fig 11.2c/d
+#%% fig 10.2c and 10.2d: Data on a circle (i.e., normalized)
+# figure-specific initializations
 radius = 0.3
 origin = [0.5, 0.5]
 std = 0.2
@@ -58,7 +54,10 @@ fig, axes = plt.subplots(nrows = 1, ncols = 2)
 circle = plt.Circle(origin, radius, edgecolor = "black", facecolor = "none")
 angle_dogs    = np.random.normal(mean_angle_dogs, std,n_dogs)
 angle_cats    = np.random.normal(mean_angle_cats, std,n_cats)
-for index in range(2):
+labels = ["before", "after"]
+
+# plotting
+for index, label in enumerate(labels):
     circ = ptch.Circle(origin, radius, color = "black", fill = False)
     if index == 0:
         cluster_mean = np.random.uniform(0,2*np.pi,2)
@@ -71,35 +70,38 @@ for index in range(2):
         axes[index].scatter(x, y, c = angle[1], marker = angle[2], s = angle[3])
     axes[index].add_patch(circ)    
     axes[index].axis([0, 1, 0, 1])
+    axes[index].set_title(label + " training")
 
-#%%
-# fig 11.3: Kohonen maps
-
+#%% fig 10.3: Kohonen maps
+# initialization
 alpha0 = 1 # initial learning rate
-beta = 1   # competition toughness 
-n_in = 3
-n_d1 = 5
-n_d2 = 5
-speed = 0.01 # to make sure the lr doesn't decrease too quickly
+beta = 1   # competition toughness (beta = inf means winner-take-all)
+n_in = 3   # number of input units
+n_d1, n_s2 = 5, 5 # size of the output grid
+speed = 0.01 # scale the shrink speed of the learning rate
 n_trials = 500
 w = np.random.uniform(0,1,(n_d1,n_d2,n_in))
-distr = [0.8, 0.1, 0.1]
+distr = [0.8, 0.1, 0.1] # biased distribution to sample a color
 
 def distance(winner,x,y):
     y_win = winner // n_d2
     x_win = winner % n_d2
     return np.sqrt((x_win-x)**2 + (y_win-y)**2)
 
+# training
 for loop in range(n_trials):
-    lrate = alpha0/(loop*speed+1)
+    lrate = alpha0/(loop*speed+1) # learning rate shrinks over time
     x = np.zeros(n_in)
-#    x[np.random.randint(0,n_in)] = 1 # from uniform distribution
-    x[np.random.choice(range(n_in), p=distr)] = 1
+#    x[np.random.randint(0,n_in)] = 1             # sample color from uniform distribution
+    x[np.random.choice(range(n_in), p=distr)] = 1 # sample color from bias distribution distr
     print(x)
     y = x.dot(np.reshape(w.swapaxes(0,2).swapaxes(1,2),(n_in,n_d1*n_d2)))
     y_max = np.argmax(y)
     for d1loop in range(n_d1):
         for d2loop in range(n_d2):
+            # update equation (10.6)
             w[d1loop,d2loop,:] = (w[d1loop,d2loop,:] + 
-               lrate*np.exp(-beta*distance(y_max,d1loop,d2loop))*(x-w[d1loop,d2loop,:])) 
+               lrate*np.exp(-beta*distance(y_max,d1loop,d2loop))*(x-w[d1loop,d2loop,:]))
+
+# plotting			
 plt.imshow(w) 
