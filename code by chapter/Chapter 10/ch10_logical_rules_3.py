6c94cffcd316df1f8f97a29c3a4b37699fff84cd
Tom Verguts <tom.verguts@ugent.be>
2022-01-06 09:43:49 +0100

    Update ch10_logical_rules_3.py

--- code by chapter/Chapter 10/ch10_logical_rules_3.py
+++ code by chapter/Chapter 10/ch10_logical_rules_3.py
@@ -5,8 +5,8 @@ Created on Thu Dec 27 15:24:48 2018
 
 @author: tom verguts
 boltzmann machine for implementing logical rules
-now it's a RESTRICTED boltzmann machine
-under construction...
+now it's a RESTRICTED boltzmann machine, with hidden units (n_components)
+under construction... hyperparameters are not optimized
 """
 
 #%% import and initialize
@@ -30,9 +30,8 @@ n_rep = 5
 #%% fit model n_rep times
 for rep_loop in range(n_rep):
     p = np.zeros((2**X.shape[1],))
-    model = BernoulliRBM(n_components = 5, n_iter = 200, batch_size = 20, learning_rate = 0.001)
+    model = BernoulliRBM(n_components = 5, n_iter = 200, batch_size = 2, learning_rate = 0.1)
     model.fit(X)
-
     # check equilibrium distribution
     for loop in range(n_test_trials):
         v_old = np.random.choice([0, 1], size = X.shape[1])
@@ -49,12 +48,12 @@ p_tot /= n_rep
 #%% print and plot
 print("overall distribution of each X pattern")
 print(p_tot)
-print("\n conditional distributions of the test patterns")
+print("\nconditional distributions of the test patterns")
 test_patterns = [[0, 0], [1, 0], [0, 1], [1, 1]]
-for test_index in range(len(test_patterns)):
+for test_pattern in test_patterns:
     prob_res = np.zeros((p_tot.shape)) # restricted probabilities
-    for p_index in range(len(p_tot)):
+    for p_index, p in enumerate(p_tot):
         prob_res[p_index]  = \
-          ((p_index%4) == (test_patterns[test_index][0]+test_patterns[test_index][1]*2)) * p_tot[p_index]
+          ((p_index%4) == (test_pattern[0]+test_pattern[1]*2)) * p
     prob_res = prob_res/np.sum(prob_res)
     print(prob_res)      
