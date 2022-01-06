61382a81c015b209a459fb5f87f93e73320f740e
Tom Verguts <tom.verguts@ugent.be>
2022-01-06 13:00:31 +0100

    more updates

--- code by chapter/Chapter 10/ch10_unsup_hebb.py
+++ code by chapter/Chapter 10/ch10_unsup_hebb.py
@@ -5,33 +5,47 @@ Created on Fri Jan 10 09:06:01 2020
 
 @author: tom verguts
 chapter 10: unsupervised learning: Unsupervised Hebbian learning rule
-Check if weights converge to first eigenvector of covariance matrix
+Check if weights converge to first eigenvector of covariance matrix (principal component (pc))
+Note: statement in MCP book that implicit (Oja) normalization is often not enough
+to keep weights within reasonable bounds, is not correct. Implicit normalization usually works well
+(just like explicit normalization does)
 """
 
+#%% import and initialize
 import numpy as np
 import matplotlib.pyplot as plt
 
-ntrial = 100
-lrate, ndim = 0.1, 2
-w = np.random.randn(2,ntrial)
-sigma = np.array([[1, -0.7], [-0.7, 1.0]])
-X = np.linalg.cholesky(sigma) # appropriate transformation matrix given covariance sigma
-data = np.ndarray((ntrial,ndim))
+n_trials = 100
+lrate, n_dims = 0.1, 2
+w = np.random.randn(2,n_trials)
+sigma = np.array([[1.0, 0.7], [0.7, 1.0]]) # covariance matrix
+eigval, eigvec = np.linalg.eig(sigma)
+pc = eigvec[:, np.argmax(eigval)]          # first principal component, explicitly computed
+X = np.linalg.cholesky(sigma)           # appropriate transformation matrix given covariance sigma
+data = np.ndarray((n_trials,n_dims))
+normalize_explicit, normalize_implicit = False, True # you can normalize explicitly and implicitly independent of each other
 fig, axs = plt.subplots(1)
-for loop in range(1, ntrial):
-    sample = np.dot(X,np.random.randn(ndim))
-    data[loop] = sample
-    y = np.dot(w[:,loop-1],sample)
-    axs.scatter(sample[0], sample[1],c = "black")
-    #w[:,loop] = w[:,loop-1] + lrate*(sample*y-(y**2)*w[:,loop]) # oja's rule
-    w[:,loop] = w[:,loop-1] + lrate*sample*y # explicit normalization
-    w[:,loop] = w[:,loop]/np.linalg.norm(w[:,loop])
 
-# plot stuff
+#%% main algorithm
+for loop in range(1, n_trials):
+    sample = np.dot(X,np.random.randn(n_dims)) # give sample the correct covariance structure
+    data[loop] = sample
+    y = np.dot(w[:,loop-1],sample) # eq (10.1)
+    axs.scatter(sample[0], sample[1],c = "black")
+    if normalize_implicit:
+        w[:,loop] = w[:,loop-1] + lrate*(sample*y-(y**2)*w[:,loop-1]) # oja's rule, eq (10.3)
+    else:		
+        w[:,loop] = w[:,loop-1] + lrate*sample*y                      # standard hebb rule, eq (10.2)
+    if normalize_explicit:
+        w[:,loop] = w[:,loop]/np.linalg.norm(w[:,loop])               # explicit normalization
+
+#%% print and plot results
 stretch = 5
 axs.set_xlim(-3, +3)
 axs.set_ylim(-3, +3)
 axs.set_xlabel("Dimension 1")
 axs.set_ylabel("Dimension 2")
-axs.plot(stretch*np.array([-w[0, ntrial-1], w[0, ntrial-1]]), stretch*np.array([-w[1, ntrial-1], w[1, ntrial-1]]), c = "black")
+axs.plot(stretch*np.array([-w[0, n_trials-1], w[0, n_trials-1]]), stretch*np.array([-w[1, n_trials-1], w[1, n_trials-1]]), c = "black")
+axs.plot(stretch*np.array([-pc[0], pc[0]]), stretch*np.array([-pc[1], pc[1]]), c = "red")
+axs.set_title("black line is principal component (pc) estimated by model; \nred line is analytically calculated pc")
 print(np.cov(data, rowvar = False)) # to check if data structure is correct; should be similar to sigma
--- code by chapter/Chapter 12/ch12_figs.py
+++ code by chapter/Chapter 12/ch12_figs.py
@@ -5,15 +5,18 @@ Created on Sun Mar  3 20:42:02 2019
 
 @author: tom verguts
 chapter 12: interacting organisms
-Specifically, interacting fireflies with the Kuramoto model
-The fireflies have disappeared from the chapter, but could stil be of interest
+Specifically, interacting fireflies with the Kuramoto model (for overview,
+see Strogatz, 2000, Physica D)
+Illustrates gradual convergence of luminance in fireflies
+also called flashing in synchrony: actual wave is not sinusoid though, this is an approximation
+The fireflies have disappeared from the chapter, but code could stil be of interest
 """
 
+#%% import and initialization
 import numpy as np
 import matplotlib.pyplot as plt
 
-## figure 12.1
-K = 0.3
+K = 0.3 # coupling strenght between the oscillators
 n_elem = 1000
 delta = 0.05
 colors = ["black", "blue", "red", "green"]
@@ -27,29 +30,29 @@ steps = np.array(range(n_elem-1)) + 1
 points = np.linspace(0, delta*n_elem, num = n_elem)
 fig, axes = plt.subplots(nrows = 2, ncols = 1)
 
-## figure 12.1a
+#%% a: sine waves
 for loop in range(len(w)):
     theta[loop, :] = w[loop]*points + np.random.rand()*(2*np.pi)
     c[loop, :] = np.sin(theta[loop,:]+phase[loop])
     axes[0].plot(points, c[loop, :], color = colors[loop])
 axes[0].set_ylabel("luminance")
-axes[0].set_title("12.1a")
+axes[0].set_title("sine waves")
 
-## figure 12.1b
+#%% b: converging oscillators in Kuramoto model
 theta = np.zeros((len(w), n_elem)) # location
 theta[:,0] = np.random.rand(len(w))*(2*np.pi) # initial location
 
 for time_loop in steps:
     for loop in range(len(w)):
-        sum = 0
-        for j_loop in range(len(w)):
-            sum += np.sin(theta[j_loop, time_loop-1]-theta[loop, time_loop-1])
-        sum *= K/len(w)    
-        theta[loop, time_loop] = theta[loop, time_loop-1] + delta*(sum+w[loop])
+        total = 0
+        for j_loop in range(len(w)): # push-pull with all the other "fireflies"
+            total += np.sin(theta[j_loop, time_loop-1]-theta[loop, time_loop-1])
+        total *= K/len(w)    
+        theta[loop, time_loop] = theta[loop, time_loop-1] + delta*(total + w[loop]) # eq (3.1) in Strogatz
         c[loop, time_loop] = np.sin(theta[loop, time_loop]+phase[loop])      
 
 for loop in range(len(w)):
     axes[1].plot(points, c[loop, :], color = colors[loop])
 axes[1].set_ylabel("luminance")
 axes[1].set_xlabel("time")
-axes[1].set_title("12.1b")
+axes[1].set_title("converging oscillators")
