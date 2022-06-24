28337f53c2a07ba534ba4b7e319eec51dccf87ce
Tom Verguts <tom.verguts@ugent.be>
2022-06-24 08:39:52 +0200

    update taxi

--- code_by_chapter/Chapter_8/ch8_tf2_taxi.py
+++ code_by_chapter/Chapter_8/ch8_tf2_taxi.py
@@ -5,6 +5,7 @@ Created on Wed Jun 22 08:38:02 2022
 
 @author: tom verguts
 solves the taxi problem using policy gradient (-like) algorithm
+under construction
 """
 #%% import, initialization, definitions
 import gym
@@ -12,64 +13,73 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 from ch8_tf2_pole_1 import perform
-
-
+import os
+import tensorflow.keras.backend as K
+from scipy.special import softmax
 
 class PG_Agent(object):
-    def __init__(self, n_states, n_actions, lr, gamma):
+    def __init__(self, n_states, n_actions, lr, gamma, max_n_step):
         self.n_states = n_states
         self.n_actions = n_actions
         self.actions = np.arange(n_actions)
         self.lr = lr
         self.gamma = gamma
-        self.network = build_network(self.n_states, self.n_actions, self.lr)
+        self.max_n_step = max_n_step
+        self.x_buffer = np.zeros((self.max_n_step, 1))
+        self.y_buffer = np.zeros((self.max_n_step, 2))
+        self.network = self.build_network()
 
     def build_network(self):
-    	def PG_loss(y_true, y_pred):
-            action_true = y_true[:, :action_dim]
-            advantage = y_true[:, action_dim:]
-            return -np.log(y_pred.prob(action_true) + 1e-5) * advantage
+        def PG_loss(y_true, y_pred):
+            action_true = K.cast(y_true[:, 0], "int32")
+            advantage =   y_true[:, 1]
+            sum_total = 0 # clunky workaround bcs direct solution didn't work...
+            for loop in range(self.max_n_step):
+                sum_total +=  advantage[loop]*K.log(y_pred[loop, action_true[loop]])
+            return -sum_total
+#           return -K.log(y_pred[: , action_true] + 1e-5) * advantage
 
         model = tf.keras.Sequential([ 
-                tf.keras.layers.Dense(action_dim, activation = "linear", name = "layer")
+                tf.keras.layers.Dense(self.n_actions, input_shape = (self.n_states,), activation = "linear", name = "layer")
 		    	] )
         model.build()
-        loss = {"layer": PG_loss()}
         model.compile(optimizer = \
-         tf.keras.optimizers.Adam(learning_rate = learning_rate), loss = loss)
+         tf.keras.optimizers.Adam(learning_rate = self.lr), loss = PG_loss)
         return model
  
-    def empty_buffer():
-		self.x_buffer = np.zeros((max_nstep, self.n_states))
-		self.y_buffer[:, actions] = np.zeros((max_nstep, self.n_actions + 1))		
+    def empty_buffer(self):
+        self.x_buffer = np.zeros((self.max_n_step, 1))
+        self.y_buffer = np.zeros((self.max_n_step, 2))		
 		
-    def update_buffer(n_step, states, actions, rewards):
-		self.x_buffer = states
-		for indx, el in actions:
-			self.y_buffer[indx, el] = 1
-		for indx in n_step:
+    def update_buffer(self, n_step, states, actions, rewards):
+        self.x_buffer = states
+        self.y_buffer[:, 0] = np.squeeze(actions).astype(int)
+        for indx in range(n_step):
             weighted_reward = 0
-			gamma_w = self.gamma # gamma raised to some power
-			for loop in np.arange(indx, n_step):
-    		    weighted_reward += gamma_w*rewards[loop]
-				gamma_w *= self.gamma
-  		    self.y_buffer[indx, -1] = weighted_reward
+            gamma_w = 1 # re_initialize
+            for loop in np.arange(indx, n_step):
+                weighted_reward += gamma_w*rewards[loop]
+                gamma_w *= self.gamma # discount
+            self.y_buffer[indx, 1] = weighted_reward
 		
     def learn(self, verbose: bool = True):
-       	self.network.fit(self.x_buffer, self.y_buffer, verbose = 0)	
+        state_array = np.zeros((self.max_n_step, self.n_states))
+        state_array[list(range(self.max_n_step)), self.x_buffer.astype(int)] = 1
+       	self.network.train_on_batch(state_array, self.y_buffer)	
         if verbose:
-			print("something")
-           
+            print("what do you want to see?")
+ 
     def sample(self, state):
-        y = self.network.predict(np.array(state[np.newaxis,:]))
-        prob = np.scipy.softmax(y)
-        action = np.choice(self.actions, prob) 
+        state_array = np.zeros((1, self.n_states))
+        state_array[0, state] = 1
+        y = self.network.predict(state_array)
+        prob = np.squeeze(softmax(y))
+        action = np.random.choice(self.actions, p = prob) 
         return action
 
-
+    
 def learn_w(env, n_loop: int = 100, max_n_step: int = 200, input_dim: int = 4):
     lc = np.zeros(n_loop)
-    buffer_count = 0
     stop_crit = False
     loop, success = 0, 0
     # learn
@@ -77,20 +87,21 @@ def learn_w(env, n_loop: int = 100, max_n_step: int = 200, input_dim: int = 4):
         print("episode loop", loop)
         n_step, done = 0, False
         state = env.reset()
-		states  = np.zeros((max_n_step, env.input_space.shape[0]))
+        states  = np.zeros((max_n_step, 1))
         actions = np.zeros((max_n_step, 1)) # to construct y_buffer
-		rewards = np.zeros((max_n-step, 1))
-        for t in range(max_per_episode):
+        rewards = np.zeros((max_n_step, 1)) # to construct y_buffer
+        for t in range(max_n_step):
             action = rl_agent.sample(state)
             next_state, reward, done, info = env.step(action)
-            states[n_step, :]  = state
-            actions[n_step, :] = action
-            rewards[n_step, :] = reward
+            states[n_step]  = state
+            actions[n_step] = action
+            rewards[n_step] = reward
             n_step += 1
-			if done:
-			    break
+            state = next_state
+            if done:
+                break
         rl_agent.empty_buffer()
-		rl_agent.update_buffer(n_step, states, actions, rewards)
+        rl_agent.update_buffer(n_step, states, actions, rewards)
         rl_agent.learn(verbose = False)
         lc[loop] = n_step
         loop += 1
@@ -103,17 +114,17 @@ def learn_w(env, n_loop: int = 100, max_n_step: int = 200, input_dim: int = 4):
 if __name__ == "__main__":
     env = gym.make('Taxi-v2')
     load_model, save_model, train_model = False, False, True
-    rl_agent = PG_Agent(env.observation_space.shape[0], env.action_space.n, \
-                           lr = 0.1, gamma = 0.9)
+    rl_agent = PG_Agent(n_states = env.observation_space.n, n_actions = env.action_space.n, \
+                           lr = 0.1, gamma = 0.9, max_n_step = 100)
     if load_model:
         rl_agent.network = tf.keras.models.load_model(os.getcwd()+"/model_taxi")
     if train_model:
-        lc, solved = learn_w(env, n_loop = 100, max_n_step = 200, input_dim = env.observation_space.shape[0])
+        lc, solved = learn_w(env, n_loop = 20, input_dim = env.observation_space.n, max_n_step = rl_agent.max_n_step)
     if save_model:
         tf.keras.models.save_model(rl_agent.network, os.getcwd()+"/model_taxi")
     if train_model:
         plt.plot(lc)
     if train_model and solved:
         print("Problem solved.")
-    perform(env, rl_agent, verbose = False)
+#    perform(env, rl_agent, verbose = False)
     env.close()
