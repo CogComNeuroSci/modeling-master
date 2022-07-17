b9cba3307581a5ac701f983f35b34b8d85916335
Tom Verguts <tom.verguts@ugent.be>
2022-07-17 17:50:23 +0200

    updates

--- code_by_chapter/Chapter_9/ch9_pacman.py
+++ code_by_chapter/Chapter_9/ch9_pacman.py
@@ -11,7 +11,16 @@ import gym, time
 env = gym.make("MsPacman-v0")
 
 env.render()
-time.sleep(1)
-# do something interesting here :-)
+state = env.reset()
+done = False
+max_n_step = 300
+n_step = 0
+
+while not done and n_step < max_n_step:
+    env.render()
+    action = env.action_space.sample()
+    state, reward, done, info = env.step(action)
+    n_step += 1 
+    time.sleep(0.1)
 
 env.close()
