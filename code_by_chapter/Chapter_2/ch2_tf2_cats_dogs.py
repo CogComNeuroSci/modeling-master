91022342568e5837f60ed032ce2c24698ad8227c
Tom Verguts <tom.verguts@ugent.be>
2022-09-23 15:02:44 +0200

    Update ch2_tf2_cats_dogs.py

--- code_by_chapter/Chapter_2/ch2_tf2_cats_dogs.py
+++ code_by_chapter/Chapter_2/ch2_tf2_cats_dogs.py
@@ -15,17 +15,18 @@ import matplotlib.pyplot as plt
 fig, ax = plt.subplots()
 
 # initialize variables
-#x_cat = np.array([1, 1, 0]) # prototypical cat
+x_cat = np.array([1, 1, 0]) # prototypical cat
 x_dog = np.array([0, 1, 1]) # prototypical dog
 x = x_dog
-W = np.array([[2, 1, 0], [0, 1, 2]])
+
+W = np.array([[1, 0.5, 0.1], [0.1, 0.5, 1]])
 net = np.matmul(W, x).reshape(2,1) # net input to the cat and dog output units
 w_inh = -0.4    # lateral inhibition between cat and dog
 W_inh = w_inh*np.array([[0, 1], [1, 0]])
 step_size = 0.05
 n_steps = 100
 y = np.ndarray((n_steps, 2))
-noise = 0.5
+noise = 0.1
 
 # define TensorFlow components
 Y  = tf.Variable(initial_value = np.random.randn(1, 2))
@@ -46,7 +47,7 @@ def plot_activation():
 for step in range(n_steps):
     Y.assign(Y + np.random.rand(1,2)*noise) # add some noise
     y[step] = Y.numpy()                     # store it as numpy array for plotting purposes
-    opt = gradient_descent.GradientDescentOptimizer(step_size).minimize(cost) # core of the code
+    gradient_descent.GradientDescentOptimizer(step_size).minimize(cost) # core of the code
     Y.assign(tf.math.maximum(0, Y))     # clip it at zero
 
 plot_activation()
