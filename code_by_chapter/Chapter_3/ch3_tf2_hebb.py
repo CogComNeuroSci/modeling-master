9bfc0b56581d92d424c5affc35eae5f43e0c4723
Tom Verguts <tom.verguts@ugent.be>
2022-10-10 08:47:36 +0200

    updates

--- code_by_chapter/Chapter_3/ch3_tf2_hebb.py
+++ code_by_chapter/Chapter_3/ch3_tf2_hebb.py
@@ -12,24 +12,24 @@ import numpy as np
 np.set_printoptions(precision = 2)
 # initialize  variables
 train_x = np.array([[1, 1, 0], [0, 1, 1]])
-train_y = np.array([[1, 0], [0, 1]])
+train_t = np.array([[1, 0], [0, 1]]) # t for target
 epochs = 20
 learning_rate = 0.1
 
 # define TensorFlow components
 X = tf.Variable(initial_value = np.random.randn(1, train_x.shape[1]).astype(np.float32), name = "input")
-Y = tf.Variable(initial_value = np.random.randn(1, train_y.shape[1]).astype(np.float32), name = "output")
-W = tf.Variable(initial_value = (np.random.randn(train_x.shape[1], train_y.shape[1])/100).astype(np.float32), name = "weights")
+T = tf.Variable(initial_value = np.random.randn(1, train_t.shape[1]).astype(np.float32), name = "output")
+W = tf.Variable(initial_value = (np.random.randn(train_x.shape[1], train_t.shape[1])/100).astype(np.float32), name = "weights")
 
 def cost():
-    return tf.matmul(-tf.matmul(X, W), tf.transpose(Y)) # this cost function (eq (3.1) in MCP book) will be optimized
+    return tf.matmul(-tf.matmul(X, W), tf.transpose(T)) # this cost function (eq (3.1) in MCP book) will be optimized
 
 opt  = tf.keras.optimizers.SGD(learning_rate = learning_rate)
 
 for epoch in range(epochs):
-    for (x, y) in zip(train_x, train_y):
+    for (x, t) in zip(train_x, train_t):
         X.assign(x[np.newaxis,:])
-        Y.assign(y[np.newaxis,:])
+        T.assign(t[np.newaxis,:])
         opt.minimize(cost, [W])
         if not epoch%10: # plot output only every 10 epochs
             w = W.numpy()
