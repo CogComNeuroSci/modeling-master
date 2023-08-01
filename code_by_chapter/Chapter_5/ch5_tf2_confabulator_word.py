90155237a86e269052d8c54c43f2f7cd9f66bcb1
Tom Verguts <tom.verguts@ugent.be>
2023-08-01 14:57:05 +0200

    Update ch5_tf2_confabulator_word.py

--- code_by_chapter/Chapter_5/ch5_tf2_confabulator_word.py
+++ code_by_chapter/Chapter_5/ch5_tf2_confabulator_word.py
@@ -11,6 +11,7 @@ Created on Sat Feb 18 10:25:11 2023
 - put train_it and save_it to False if you want to test an existing model
 - you can make the path deterministic (always the same) by setting the random seed;
 - you can make sure you sample the same word in the same situation by sampling via argmax;
+- var max_line is useful to speed up the process (the word version is slow with all the text)
 try it out and try to predict what will be different
 """
 
@@ -47,22 +48,23 @@ def test_model(n_cont = 50):
     x = np.zeros((batch_size, stim_depth, stim_dim))
     r = np.random.randint(0, len(data)- n_seed)
     seed = data[r:r+n_seed]
-    words_generated = [itos[tok] for tok in seed]
+    words = [itos[tok] for tok in seed]
     for loop in range(n_seed):
         x[0, loop, seed[loop]] = 1
     for row in range(n_seed-1, n_seed + n_cont):
         out = model(x)
         batch_nr, stim_nr = row//stim_depth, row%stim_depth
         probs = np.ndarray.flatten(out[batch_nr, stim_nr, :].numpy())
-        y = np.random.choice(np.arange(len(words)), p = probs)
+        y = np.random.choice(np.arange(len(chars)), p = probs)
 #        y = np.argmax(probs)		# sample the highest prob word
-        words_generated.append(itos[y])
+        words.append(itos[y])
         batch_nr, stim_nr = (row+1)//stim_depth, (row+1)%stim_depth
         x[batch_nr, stim_nr, y] = 1        
-    print(" ".join(words_generated[n_seed:]), "\n")
+    print(" ".join(words[n_seed:]), "\n")
 
-def text2vec(text_file, verbose = False, start_line = None):
+def text2vec(text_file, verbose = False, start_line = None, max_line = None):
     """preprocessing of the data"""
+    line_nr = 0
     include = False
     words = set()
     data_length = 0
@@ -72,6 +74,9 @@ def text2vec(text_file, verbose = False, start_line = None):
             if (line == start_line) or not start_line:
 	            include = True 
             if include:
+                line_nr += 1
+                if line_nr == max_line:
+                    break
                 for word in line.split():
                     banned = False
                     for char in banned_chars:
@@ -89,11 +94,15 @@ def text2vec(text_file, verbose = False, start_line = None):
     data = np.zeros(data_length, dtype=np.int)
     i = 0
     include = False
+    line_nr = 0
     with open(text_file, 'r') as infile:
         for line in infile:
             if (line == start_line) or not start_line:
                 include = True
             if include:
+                line_nr += 1
+                if line_nr == max_line:
+                    break
                 for word in line.split():
                     banned = False
                     for char in banned_chars:
@@ -117,19 +126,19 @@ def make_data(data, n_stim, stim_depth, stim_dim):
 
 # start main code here
 text = "shakespeare_digest.txt"
-train_it, save_it, model_nr = True, True, 3
+train_it, save_it, model_nr = True, True, 1
 
 
 if train_it: # train the model
     batch_size = 128
     data_size  = batch_size*20 # data for one model.fit()
     stim_depth = 10
-    data, words, stoi, itos = text2vec(text, verbose = True)
-    stim_dim = len(words)
+    data, chars, stoi, itos = text2vec(text, verbose = True)
+    stim_dim = len(chars)
     model = build_network(batch_size = batch_size, input_dim = stim_dim, output_dim = stim_dim, n_hid = 128)
     print("pre training:")
     test_model(n_cont = 20)
-    res = train_model(n_times = 5, test_it = True) # the length of this training determines execution time
+    res = train_model(n_times = 100, test_it = True) # the length of this training determines execution time
     plt.plot(res.history["loss"])
 else: # load the model + processed data
     savedir = join(os.getcwd(), "models_word")
@@ -138,8 +147,8 @@ else: # load the model + processed data
     batch_size = model.input.shape[0]
     stim_depth = model.input.shape[1]
     with open(join(savedir, "texts"+str(model_nr)+".pkl"), 'rb') as f:  
-        data, words, stoi, itos = pickle.load(f)
-    stim_dim = len(words)
+        data, chars, stoi, itos = pickle.load(f)
+    stim_dim = len(chars)
 	
 if save_it:
     savedir = join(os.getcwd(), "models_word")
@@ -147,7 +156,7 @@ if save_it:
         os.mkdir(savedir)
     tf.keras.models.save_model(model, os.path.join(savedir,"model_shake"+str(model_nr)+".keras"), save_format = "keras")
     with open(join(savedir, "texts"+str(model_nr)+".pkl"), 'wb') as f:  
-        pickle.dump([data, words, stoi, itos], f)
+        pickle.dump([data, chars, stoi, itos], f)
 
 print("post training:") # a walk in word space
 #np.random.seed(2002)
