460206c6920a6d00d870feac75e31f7c30882b31
Tom Verguts <tom.verguts@ugent.be>
2024-09-07 14:03:05 +0200

    ch 5 updates

--- code_by_chapter/Chapter_5/ch5_tf2_face_classif_conv.py
+++ code_by_chapter/Chapter_5/ch5_tf2_face_classif_conv.py
@@ -19,13 +19,12 @@ from process_faces import load_faces_labels, show_face
 
 
 # downscale to make data set smaller (and training faster)
-train_size = 50
+train_size = 70
 granularity = 5 # to downsample the image
 
 x_train, y_train, x_test, y_test = load_faces_labels("CFD-Version-3.0/Images/CFD-INDIA", 
-					 gran = granularity, n_faces = train_size, test = 0.3, depth = 1)
-show_face(x_train[0]) # plot a random face
-
+					 gran = granularity, n_faces = train_size, test = 0.3, depth = 3)
+show_face(x_train[0:9], 3, 3, labels = y_train[0:9], title = "real labels") # plot random faces
 learning_rate = 0.00002
 epochs = 100
 batch_size = 10
@@ -42,6 +41,8 @@ y_train = tf.one_hot(y_train, n_labels) # one-hot coding (e.g., 3 becomes (0, 0,
 model = tf.keras.Sequential([
 			tf.keras.layers.Conv2D(filters = 16, kernel_size = (3, 3), input_shape = (x_train.shape[1], x_train.shape[2], x_train.shape[3])),
 			tf.keras.layers.AveragePooling2D(pool_size = (2, 2)),
+			tf.keras.layers.Conv2D(filters = 32, kernel_size = (3, 3)),
+			tf.keras.layers.AveragePooling2D(pool_size = (2, 2)),
 			tf.keras.layers.Flatten(),
 			tf.keras.layers.Dense(10, activation = "relu"),
 			tf.keras.layers.Dense(10, activation = "relu"),
@@ -52,13 +53,18 @@ loss = tf.keras.losses.CategoricalCrossentropy()
 #opt = tf.keras.optimizers.Adam(learning_rate = learning_rate)
 opt = tf.keras.optimizers.legacy.Adam(learning_rate = learning_rate) # if you have an M1/M2 processor (like me)
 model.compile(optimizer = opt, loss = loss)
-print("number of parameters= ", model.count_params())
-
+#print("number of parameters= ", model.count_params())
+#print(model.summary())
 #%% model fitting
+# plot before training
+y_pred = model.predict(x_test)
+show_face(x_test[0:9], 3, 3, labels = y_pred[0:9], title = "predictions before training") # plot faces before training
 history = model.fit(x_train, y_train, batch_size = batch_size, epochs = epochs)
 
 #%% show results
 # error curve
+y_pred = model.predict(x_test)
+show_face(x_test[0:9], 3, 3, labels = y_pred[0:9], title = "predictions after training") # plot faces after training
 fig = plt.figure()
 plt.plot(history.history["loss"], color = "black")
 
--- code_by_chapter/Chapter_5/process_faces.py
+++ code_by_chapter/Chapter_5/process_faces.py
@@ -15,7 +15,8 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 
 
-def plot_faces(generator, n: int = 15, stim_row: int = 172, stim_col: int = 245, show: bool = True, dims: tuple = (0, 1)):
+def plot_faces(generator, n: int = 15, stim_row: int = 172, stim_col: int = 245, show: bool = True, dims: tuple = (0, 1), n_input: int = 1):
+	# n_input concerns the input filters
 	# display a 2D manifold of the stimuli (e.g., digits)
 	input_dim = generator.inputs[0].shape[1]
 	n = 5
@@ -23,7 +24,10 @@ def plot_faces(generator, n: int = 15, stim_row: int = 172, stim_col: int = 245,
 		n1 = n  # figure with n x n digits
 	else:
 		n1 = 1
-	figure = np.zeros((stim_row * n1, stim_col * n))
+	if n_input == 1:
+		figure = np.zeros((stim_row * n1, stim_col * n))
+	else:
+		figure = np.zeros((stim_row * n1, stim_col * n, n_input))
 	# linearly spaced coordinates on the unit square were transformed through the inverse CDF (ppf) of the Gaussian
 	# to produce values of the latent variables z, since the prior of the latent space is Gaussian
 	grid_x = norm.ppf(np.linspace(0.05, 0.95, n))
@@ -33,29 +37,45 @@ def plot_faces(generator, n: int = 15, stim_row: int = 172, stim_col: int = 245,
 		for i, yi in enumerate(grid_x):
 			for j, xi in enumerate(grid_y):
 				z_sample = np.zeros((1, input_dim))
-#				data = np.array([[xi, yi]])
 				z_sample[0, dims[0]] = xi
 				z_sample[0, dims[1]] = yi
-#				if input_dim > 2:
-#					z_sample = np.column_stack((z_sample, np.zeros((1, input_dim-2))))
 				x_decoded = generator.predict(z_sample, verbose = 0)
-				face = x_decoded[0].reshape(stim_row, stim_col)
-				figure[i * stim_row: (i + 1) * stim_row,
-		               j * stim_col: (j + 1) * stim_col] = face
+				if n_input == 1:
+					face = x_decoded[0].reshape(stim_row, stim_col)
+					figure[i * stim_row: (i + 1) * stim_row,
+			               j * stim_col: (j + 1) * stim_col] = face
+				else:
+					face = x_decoded[0].reshape(stim_row, stim_col, n_input)
+					figure[i * stim_row: (i + 1) * stim_row,
+		               j * stim_col: (j + 1) * stim_col, :] = face
 	else:
 		for i, yi in enumerate(grid_x):
 			z_sample = np.array([[yi]])
 			x_decoded = generator.predict(z_sample, verbose = 0)
-			face = x_decoded[0].reshape(stim_row, stim_col)
-			figure[0: stim_row,
-                i * stim_col: (i + 1) * stim_col] = face
+			if n_input == 1:
+				face = x_decoded[0].reshape(stim_row, stim_col)
+				figure[0: stim_row,
+	                i * stim_col: (i + 1) * stim_col] = face
+			else:
+				face = x_decoded[0].reshape(stim_row, stim_col, n_input)
+				figure[0: stim_row,
+	                i * stim_col: (i + 1) * stim_col, :] = face
 	if show:
 		plt.figure(figsize=(10, 10))
 		plt.imshow(figure, cmap='Greys_r')
 		plt.show()
 	return figure	
 
-def load_faces(dirs, gran = 10, n_faces = 2):
+def plot_face(generator):
+# plot a single randomly generated face
+	input_dim = generator.inputs[0].shape[1]
+	z_sample = np.random.normal(0, 1, size = (1, input_dim))
+	face = generator.predict(z_sample, verbose = 0)[0]
+	plt.figure()
+	plt.imshow(face)
+
+
+def load_faces(dirs, gran = 10, n_faces = 2, n_filter = 1):
 # gran = granularity, amount of downsampling of the stimuli
 	dirs = path.join(getcwd(), dirs)
 	chdir(dirs)
@@ -68,7 +88,10 @@ def load_faces(dirs, gran = 10, n_faces = 2):
 		sample += 1
 		if file[-3:] == "jpg":
 			n_face += 1
-			faces.append(imread(file)[::gran,::gran,0])
+			if n_filter == 1:
+				faces.append(imread(file)[::gran,::gran,0])
+			else:
+				faces.append(imread(file)[::gran,::gran,:n_filter])
 		if n_face == n_faces:
 			break
 	faces = np.array(faces)	
@@ -113,10 +136,67 @@ def load_faces_labels(dirs, gran = 10, n_faces = 2, test = 0.2, depth = 1):
 	del faces				
 	return faces_train, labels_train, faces_test, labels_test
 
-def show_face(face):
-	plt.figure(figsize=(10, 10))
-	plt.imshow(face, cmap='Greys_r')
-	plt.show()
+def load_celebs(dirs, gran = 10, n_faces = 2, test = 0.2, depth = 1):
+# loading the celeb dataset
+# gran = granularity, amount of downsampling of the stimuli
+	dirs = path.join(getcwd(), dirs)
+	chdir(dirs)
+	list_faces = listdir(dirs)
+	list_faces = list_faces[:n_faces]
+	faces = []
+	for count in range(n_faces):
+		file = path.join(dirs, list_faces[count])
+		faces.append(imread(file)[::gran,::gran,:depth])
+	faces = np.array(faces)
+	if test > 0:
+		nrs = np.arange(n_faces)
+		np.random.shuffle(nrs)
+		n_train   = int(np.floor(n_faces*(1-test)))
+		faces_train = faces[nrs[:n_train]]
+		faces_test  = faces[nrs[n_train:]]
+	else:
+		faces_train = faces
+		faces_test  = faces
+	del faces				
+	return faces_train, faces_test
+
+def show_face(face, nrow = 1, ncol = 1, labels = None, title = " "):
+# show faces, possibly with labels
+	face_labels = ["female", "male"]
+	if not (labels is None):
+		if not (type(labels) == np.ndarray):
+			labels = 1 - labels.numpy()
+		if len(labels.shape)>1:
+			labels = labels[:,1]	
+		labels = labels.astype(np.int16)
+	if face.shape[0] != nrow*ncol:
+		print("dimension mismatch")
+	else:
+		fig, ax = plt.subplots(nrow, ncol)
+		fig.suptitle(title)
+		if nrow == 1 and ncol == 1:
+			ax.imshow(face, cmap='Greys_r')
+			ax.set_xticks([], [])
+			if not (labels is None):
+				title = face_labels[labels]
+				ax.set_title(title)
+		elif nrow == 1 or ncol == 1:
+			for loop in range(nrow*ncol):
+				ax[loop].imshow(face[loop], cmap='Greys_r')
+				ax[loop].set_xticks([], [])
+				if not (labels is None):
+					title = face_labels[labels[loop]]
+					ax[loop].set_title(title)					
+		else:
+			for rowloop in range(nrow):
+				for colloop in range(ncol):
+					index = rowloop*ncol+colloop
+					ax[rowloop, colloop].imshow(face[index], cmap='Greys_r')
+					ax[rowloop, colloop].set_xticks([], [])
+					if not (labels is None):
+						title = face_labels[labels[index]]
+						ax[rowloop, colloop].set_title(title)					
+		plt.show()
 	
 if __name__ == "__main__":
 	# just a sanity check
